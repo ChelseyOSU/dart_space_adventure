b0ab7d5631ecd153be0d936a0d4db0decd461faf
XIAOSHI Zeng <xiaoshizeng@XIAOSHIs-MBP.attlocal.net>
2022-01-23 20:30:12 -0800

    Generate planets using mock data

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -3,7 +3,7 @@ import 'package:dart_space_adventure/dart_space_adventure.dart';
 void main(List<String> arguments) {
   SpaceAdventure(
     planetarySystem: PlanetarySystem(
-      name: 'Solar System', planets: []
+      name: 'Solar System', planets: mockPlanets()
     )
   ).start();
 }
