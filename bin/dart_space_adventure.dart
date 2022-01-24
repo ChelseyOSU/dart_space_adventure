8f7b782ff8bf79a3eb03677c7ab35de1dad51a2f
XIAOSHI Zeng <xiaoshizeng@XIAOSHIs-MBP.attlocal.net>
2022-01-23 21:09:17 -0800

    update mock data to be map

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -1,26 +1,28 @@
 import 'package:dart_space_adventure/dart_space_adventure.dart';
 
+const systemName = 'Solary System';
+const planetData = {
+  'Mercury': 'A very hot planet, closet to the sun.',
+  'Saturn': 'A very cold planet, furtherest to the sun.',
+  'Earth': 'Home, sweet home.'
+};
+
 void main(List<String> arguments) {
   SpaceAdventure(
     planetarySystem: PlanetarySystem(
-      name: 'Solar System', planets: mockPlanets()
+      name: systemName,
+      planets: mockPlanets()
     )
   ).start();
 }
 
 List<Planet> mockPlanets() {
-  return [
-    Planet(
-      name: 'Mercury',
-      description: 'A very hot planet, closet to the sun.'
-    ),
-    Planet(
-      name: 'Saturn',
-      description: 'A very cold planet, furtherest to the sun.'
-    ),
-    Planet(
-      name: 'Earth',
-      description: 'Home, sweet home.'
-    )
-  ];
+  List<Planet> planets = [];
+  planetData.forEach((key, value) {
+    var name = key;
+    var description = value;
+    var planet = Planet(name: name, description: description);
+    planets.add(planet);
+  });
+  return planets;
 }
