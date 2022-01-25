e2ddd8048f8c2187e5a6aa1a7105969eb8b8afb2
XIAOSHI Zeng <xiaoshizeng@XIAOSHIs-MBP.attlocal.net>
2022-01-24 23:02:29 -0800

    read planets data from JSON files

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -1,23 +1,9 @@
 import 'package:dart_space_adventure/dart_space_adventure.dart';
 
-const systemName = 'Solary System';
-const planetData = {
-  'Mercury': 'A very hot planet, closet to the sun.',
-  'Saturn': 'A very cold planet, furtherest to the sun.',
-  'Earth': 'Home, sweet home.'
-};
+const fileName = 'bin/planetarySystem.json';
 
 void main(List<String> arguments) {
   SpaceAdventure(
-    planetarySystem: PlanetarySystem(
-      name: systemName,
-      planets: mockPlanets()
-    )
+    planetarySystem: PlanetarySystem.fromJsonFile(fileName)
   ).start();
-}
-
-List<Planet> mockPlanets() {
-  return planetData.entries.map(
-    (entry) => Planet(name: entry.key, description: entry.value)
-  ).toList();
 }
--- lib/src/planetary_system.dart
+++ lib/src/planetary_system.dart
@@ -1,15 +1,27 @@
+import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
 import 'planet.dart';
 
 class PlanetarySystem {
   //declare member variable
   final Random _random = Random();
-  final String name;
-  final List<Planet> planets;
+  String name = "";
+  List<Planet> planets = const[];
   //[] for optional, initializing formal
   //named parameter
   //compile time constant list for planets
   PlanetarySystem({this.name = 'Unnamed System', this.planets = const[]});
+
+  //named constructor to allow reading from JSON file
+  PlanetarySystem.fromJsonFile(String fileName) {
+    Map planetarySystemMap = readPlanetarySystemFromJson(fileName);
+    List planetDataFromJson = getPlanetsFromPlanetarySystemMap(planetarySystemMap);
+    this.name = getNameFromPlanetarySystemMap(planetarySystemMap);
+    this.planets = planetDataFromJson.map(
+        (entry) => Planet(name: entry['name'], description: entry['description'])
+      ).toList();
+  }
 
   //property
   int get numberOfPlanets => planets.length;
@@ -25,5 +37,20 @@ class PlanetarySystem {
       (planet) => planet.name == name,
       orElse: () => Planet.nullPlanet()
     );
+  }
+
+  Map readPlanetarySystemFromJson(String fileName) {
+    Map planetarySystemMap = jsonDecode(new File(fileName).readAsStringSync());
+    return planetarySystemMap;
+  }
+
+  //by default returns an empty list
+  List getPlanetsFromPlanetarySystemMap(Map planetarySystemMap) {
+    return planetarySystemMap['planets'] ?? [];
+  }
+
+  //by default returns special name 'NULL'
+  String getNameFromPlanetarySystemMap(Map planetarySystemMap) {
+    return planetarySystemMap['name'] ?? 'NULL';
   }
 }
